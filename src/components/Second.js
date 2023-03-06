e83d2e9c0a27139a4721461175730069dec93220
Sakshijhamnani <sakshijhamnani@gmail.com>
2023-03-06 21:42:45 +0530

    Components Added

---
+++ src/components/Second.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Second(){
+    return(
+    <h2>I am heading two...from second component</h2>
+    );
+    
+}
---
+++ src/components/Third.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Third(){
+    return(
+    <h1>I am heading three...from third component</h1>
+    );
+    
+}
